d873265ccd66463fa29bb836df70c7b1593815cd
Ieffai <s9219871110@gmail.com>
2020-05-18 20:02:18 +0300

    burger-menu work on nativ JS instead of jQuery

---
+++ src/blocks/header/header.js
@@ -0,0 +1,26 @@
+import Component from "../component";
+
+import './header.css';
+
+export default class Header extends Component {
+  constructor (...args) {
+    super(...args);
+
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle(event) {
+    const a = document.querySelector('.header__menu');
+
+    if(event.target.closest('.header__burger')){
+      this.domElement.classList.toggle('header__burger_is-active');
+      a.classList.toggle('header__menu_show');
+    }
+    if(event.target.closest('#headerLoginBtn')){
+      this.domElement.classList.toggle('header__burger_is-active');
+      a.classList.toggle('header__menu_show');
+    }
+
+  }
+
+}
--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,18 +1,21 @@
 import "./index.css";
 
 import Popup from "../../blocks/popup/popup";
+import Header from "../../blocks/header/header";
 
 const signUp = document.getElementById('popupSignUp');
 const logIn = document.getElementById('popupLogin');
-
+const burger = document.querySelector('.header__burger');
 
 const signUpPopup = new Popup(signUp);
 const logInPopup = new Popup(logIn);
+const burgerMenu = new Header(burger);
 
 const root = document.querySelector('.root');
 
 root.addEventListener('click', () => {
   logInPopup.show(event);
+  burgerMenu.toggle(event);
   });
 
 root.addEventListener('click', () => {
@@ -20,25 +23,7 @@ root.addEventListener('click', () => {
   logInPopup.hide(event);
   });;
 
-
-
-$('.header__burger').click(function(e) {
-  e.preventDefault;
-  $(this).toggleClass('header__burger_is-active');
-});
-
-$('.header__burger').click(function(e) {
-  e.preventDefault;
-  $('.header__menu').toggleClass('header__menu_show');
-});
-
 $('.bookmark__icon').click(function(e) {
   e.preventDefault;
   $('.bookmark__icon_color').toggleClass('bookmark__icon_saved');
 });
-
-  // $( '.bookmark__icon' ).hover(function(){
-  //   $('.bookmark__tip' ).css( "display", "flex" );
-  //   }, function(){
-  //   $('.bookmark__tip' ).css( "display", "none" );
-  // });
